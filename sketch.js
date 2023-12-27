94824932ccd8cf1969ee7ad7496be0d6a88f0426
Arslan Aftab <arslan@Arslans-MBP.fritz.box>
2023-12-27 22:44:52 +0100

    check collisions

--- sketch.js
+++ sketch.js
@@ -6,16 +6,43 @@ function setup() {
     createCanvas(800, 600);
     frameRate(60);
     
-    gravitySlider = createSlider(0, 0.5, 0.1, 0.01); // Min, Max, Default, Step
+    gravitySlider = createSlider(0, 0.5, 0.1, 0.01);
     gravitySlider.position(10, 10);
     gravitySlider.style('width', '80px');
     
-    let gravityLabel = createP('Gravity'); // Create label element
+    let gravityLabel = createP('Gravity');
     gravityLabel.position(10, 10);
 }
 
 function preload() {
-    bounceSound = loadSound('sound.mp3'); // Replace with your sound file path
+    bounceSound = loadSound('sound.mp3');
+}
+
+function checkCollisions() {
+    for (let i = 0; i < balls.length; i++) {
+        for (let j = i + 1; j < balls.length; j++) {
+            let dx = balls[j].x - balls[i].x;
+            let dy = balls[j].y - balls[i].y;
+            let distance = sqrt(dx * dx + dy * dy);
+            let minDist = balls[i].radius + balls[j].radius;
+
+            if (distance < minDist) {
+                let overlap = 0.5 * (minDist - distance);
+
+                let nx = dx / distance;
+                let ny = dy / distance;
+
+                balls[i].x -= overlap * nx;
+                balls[i].y -= overlap * ny;
+                balls[j].x += overlap * nx;
+                balls[j].y += overlap * ny;
+
+                // Exchange speeds
+                [balls[i].xSpeed, balls[j].xSpeed] = [balls[j].xSpeed, balls[i].xSpeed];
+                [balls[i].ySpeed, balls[j].ySpeed] = [balls[j].ySpeed, balls[i].ySpeed];
+            }
+        }
+    }
 }
 
 function draw() {
@@ -28,17 +55,24 @@ function draw() {
     textAlign(RIGHT);
     text("FPS: " + fps.toFixed(2), width - 10, 20);
 
+    // Display ball count
+    textAlign(LEFT);
+    text("Balls: " + balls.length, 10, 20);
+    
     for (let i = 0; i < balls.length; i++) {
         let ball = balls[i];
         ball.applyGravity();
         ball.move();
         ball.display();
     }
+    checkCollisions();
 }
 
 function mousePressed() {
-    let newBall = new Ball(mouseX, mouseY);
-    balls.push(newBall);
+    if (balls.length < 10) { // Limit the number of balls
+        let newBall = new Ball(mouseX, mouseY);
+        balls.push(newBall);
+    }
 }
 
 class Ball {
@@ -47,9 +81,10 @@ class Ball {
         this.y = y;
         this.radius = 20;
         this.xSpeed = random(-2, 2);
-        this.ySpeed = 0; // Starts with no vertical speed
-        this.damping = 0.90; // Energy loss factor
-        this.gravity = 0.1; // Gravity factor
+        this.ySpeed = 0;
+        this.damping = 0.90;
+        this.gravity = 0.1;
+        this.color = color(random(255), random(255), random(255)); 
     }
 
     applyGravity() {
@@ -67,15 +102,22 @@ class Ball {
 
         // Bounce off the bottom edge
         if (this.y > height - this.radius) {
-            this.y = height - this.radius; // Reset to the bottom edge
-            this.ySpeed *= -this.damping; // Reverse speed with damping
-            if (abs(this.ySpeed) > 1) { // Check to ensure sound is not played on tiny bounces
+            this.y = height - this.radius;
+            this.ySpeed *= -this.damping;
+            if (abs(this.ySpeed) > 1) {
                 bounceSound.play();
             }
+        }
+
+        // Bounce off the top edge
+        if (this.y < this.radius) {
+            this.y = this.radius;
+            this.ySpeed *= -this.damping;
         }
     }
 
     display() {
+        fill(this.color); // Use the random color
         ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
     }
 }
